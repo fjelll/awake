37e6b3f1e2c04cd13e9e5f76efc66d5f6b73d866
fjelll <bigoilyman40@gmail.com>
2025-09-16 21:30:23 +0200

    eye

--- src/app/main-page/header/header.component.ts
+++ src/app/main-page/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { EyeComponent } from "../../eye/eye.component";
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [MatSlideToggleModule]
+  imports: [MatSlideToggleModule, EyeComponent]
 })
 export class HeaderComponent {}
