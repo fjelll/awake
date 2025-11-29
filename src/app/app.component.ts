a5c29e4f08c16303448beba0dae2d5ee7d895433
fjelll <bigoilyman40@gmail.com>
2025-11-29 02:07:22 +0100

    new desing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component } from "@angular/core";
 import { HeaderComponent } from "./main-page/header/header.component";
 import { MainContentComponent } from "./main-page/main-content/main-content.component";
-import { AccountComponent } from "./main-page/account/account.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, MainContentComponent, AccountComponent],
+  imports: [HeaderComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
-
 })
 
 
