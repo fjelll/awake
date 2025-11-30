f38464db9a88261c362315477208346bdc61da74
fjelll <bigoilyman40@gmail.com>
2025-12-01 00:47:41 +0100

    sub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from "@angular/core";
 import { HeaderComponent } from "./main-page/header/header.component";
 import { MainContentComponent } from "./main-page/main-content/main-content.component";
+import { SubscriptionComponent } from "./subscription/subscription.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, MainContentComponent],
+  imports: [HeaderComponent, MainContentComponent, SubscriptionComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
---
+++ src/app/subscription/subscription.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-subscription',
+  imports: [MatCardModule],
+  templateUrl: './subscription.component.html',
+  styleUrl: './subscription.component.scss'
+})
+export class SubscriptionComponent {
+
+}
