9ba44af2bf1a01e059b3d6b988aa9f86507c4b8b
fjelll <bigoilyman40@gmail.com>
2025-12-10 20:45:02 +0100

    subscription

--- src/app/subscription/subscription.component.ts
+++ src/app/subscription/subscription.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import subscriptionData from '../../subscriptions.json';
+
+interface SubscriptionPlan {
+  title: string;
+  price: string | number;
+  months: number | string;
+  features: string[];
+}
 
 @Component({
   selector: 'app-subscription',
-  imports: [MatCardModule],
+  imports: [MatCardModule, CommonModule],
+  standalone: true,
   templateUrl: './subscription.component.html',
   styleUrl: './subscription.component.scss'
 })
 export class SubscriptionComponent {
-
+  plans: SubscriptionPlan[] = subscriptionData.plans;
 }
