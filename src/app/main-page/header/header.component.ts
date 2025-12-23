e7bb32a0231983212b54af0e0813f030a88de710
fjelll <bigoilyman40@gmail.com>
2025-12-23 18:04:58 +0100

    s

--- src/app/main-page/header/header.component.ts
+++ src/app/main-page/header/header.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [MatSlideToggleModule]
+  imports: [MatSlideToggleModule],
 })
 export class HeaderComponent {}
